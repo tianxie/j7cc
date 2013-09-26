2480a4f6f999ddd8609c0e8997ae759e4e0c248d
Tian Xie <zjuxietian@gmail.com>
2013-09-26 19:51:22 +0800

    Commit ch1_recipe07

---
+++ src/ch1_recipe07/Event.java
@@ -0,0 +1,28 @@
+package ch1_recipe07;
+
+import java.util.Date;
+
+/**
+ * Date: 13-9-26
+ * Time: 19:19
+ */
+public class Event {
+    private Date date;
+    private String event;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+}
---
+++ src/ch1_recipe07/Main.java
@@ -0,0 +1,21 @@
+package ch1_recipe07;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Date: 13-9-26
+ * Time: 19:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        Deque<Event> deque = new ArrayDeque<>();
+        WriterTask writer = new WriterTask(deque);
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(writer);
+            thread.start();
+        }
+        CleanerTask cleaner = new CleanerTask(deque);
+        cleaner.start();
+    }
+}
